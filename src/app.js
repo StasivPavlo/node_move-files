ca83c75ae59c7b6259c330401bc42bba354d9c99
StasivPavlo <pavlostasiv@gmail.com>
2024-11-07 17:15:28 +0100

    improved

--- src/app.js
+++ src/app.js
@@ -5,31 +5,34 @@ const path = require('path');
 
 // eslint-disable-next-line no-shadow
 function moveFile(currentPath, newPath) {
-  const fileData = fs.readFileSync(currentPath, 'utf-8');
-
-  if (newPath.endsWith('/')) {
-    fs.writeFileSync(newPath + path.basename(currentPath), fileData);
-  } else {
-    if (newPath.includes('/')) {
-      fs.writeFileSync(newPath, fileData);
-    } else {
-      if (fs.existsSync(newPath + '/')) {
-        fs.writeFileSync(newPath + '/' + path.basename(currentPath), fileData);
-      } else {
-        fs.renameSync(currentPath, newPath);
-
-        return;
-      }
-    }
+  if (!fs.existsSync(currentPath)) {
+    throw new Error(`Source file '${currentPath}' does not exist.`);
   }
 
-  fs.unlinkSync(currentPath);
-};
+  const fileData = fs.readFileSync(currentPath, 'utf-8');
+  let targetPath;
+
+  if (newPath.endsWith('/')) {
+    targetPath = newPath + path.basename(currentPath);
+  } else if (newPath.includes('/')) {
+    targetPath = newPath;
+  } else if (fs.existsSync(newPath + '/')) {
+    targetPath = newPath + '/' + path.basename(currentPath);
+  } else {
+    fs.renameSync(currentPath, newPath);
+
+    return;
+  }
+
+  // Write file data to the determined target path
+  fs.writeFileSync(targetPath, fileData);
+  fs.unlinkSync(currentPath); // Remove the original file after moving
+}
 
 const [currentPath, newPath] = process.argv.slice(2);
 
-if (!fs.existsSync(newPath) && newPath.includes('/')) {
-  throw new Error(`this directory '${newPath}' does not exist.`);
+if (!fs.existsSync(path.dirname(newPath)) && newPath.includes('/')) {
+  throw new Error(`The directory '${path.dirname(newPath)}' does not exist.`);
 }
 
 moveFile(currentPath, newPath);
